d3372e30df9d6de7ffea640de580246fcf190e65
MykolaPMP <31858361+MykolaPMP@users.noreply.github.com>
2021-03-30 16:09:41 +0300

    Init commit
    Contains database objects in DTO project.
    Contains crawler in DataRetriever project.

---
+++ NewsPageCrawler/DataRetriever/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DTO;
+
+namespace DataRetriever
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TheVergeCrawler vergeCrawler = new TheVergeCrawler();
+            ViceCrawler viceCrawler = new ViceCrawler();
+
+
+
+            ICrawl crawler = viceCrawler;
+            var list = crawler.Crawl();
+
+            MongoCRUD db = new MongoCRUD("NewsCrawl");
+
+            foreach (var title in list)
+            {
+                db.InsertRecord("News", title);
+                Console.WriteLine("Title: " + title.Title + Environment.NewLine
+                    + "Url: " + title.Url + Environment.NewLine
+                    + "Author: " + title.Author + Environment.NewLine
+                    + "Date of publication: " + title.DateOfPublication + Environment.NewLine);
+
+            }
+
+
+            Console.WriteLine("");
+        }
+    }
+}
